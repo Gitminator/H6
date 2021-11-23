c0922411e4e87889af00b89b5c9c0438101a2d17
Jens Hansen <mudsale@hotmail.com>
2021-11-23 14:24:56 +0100

    API Folder structure

---
+++ API/src/main/java/eu/bb/bbeubackend/HelloApplication.java
@@ -0,0 +1,9 @@
+package eu.bb.bbeubackend;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
